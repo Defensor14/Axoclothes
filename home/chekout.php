761171d4ce0a49843c3a6a1ded6aa72438575cc8
AaronGomezC <168867785+AaronGomezC@users.noreply.github.com>
2024-05-28 08:40:09 -0600

    Update chekout.php
    -Tamano de seccion
    -Ortografia

--- home/chekout.php
+++ home/chekout.php
@@ -133,7 +133,7 @@ if ($productos != null) {
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    ¿Seguro que deseas eliminar?
+                    ¿Seguro Que Deseas Eliminar?
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
