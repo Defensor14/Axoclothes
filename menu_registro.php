219cfc7b689e7b97c8c63770863f6592c7960cbe
AaronGomezC <168867785+AaronGomezC@users.noreply.github.com>
2024-05-28 09:02:07 -0600

    menu_registro
    -Usuario tiene opcion de regresar a la pagina anterior

---
+++ menu_registro.php
@@ -0,0 +1,13 @@
+<nav class="navbar">
+        <div id="logo">
+            <img src="images/logo.svg">
+        </div>
+        <a class="navbar-brand" href="index.php">AXOCLOTHES</a>
+        <button class="navbar-toggler btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <spanclass="navbar-toggler-icon"></span></button>
+        <ul class="menu">
+            <li><a href="index.php">Inicio</a></li>
+            <li><a href="window.history.back()">Regresar</a></li> 
+        </ul>
+    </nav>
