74b0f1f630131b4de5dc7199826f2b279dabdb69
AaronGomezC <168867785+AaronGomezC@users.noreply.github.com>
2024-05-28 08:42:57 -0600

    Update detalles.php
    -Agregar el menu.php

--- home/detalles.php
+++ home/detalles.php
@@ -79,53 +79,8 @@ if($id == '' || $token == ''){
 
 <body>
     <!-- Navigation-->
-    <nav class="navbar">
-        <div id="logo">
-            <img src="images/logo.svg" style="width: 30px; margin: 5px;">
-        </div>
-        <a class="navbar-marca" href="index.php">AXOCLOTHES</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-            aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-            
-            <ul class="menu">
-                <li><a href="index.php">Inicio</a></li>
-                <li><a href="inventario.php">Inventario</a></li>
-                <li class="dropdown">
-                    <a href="AllProducts.php"class="dropbtn">Comprar</a>
-                    <div class="dropdown-content">
-                        <a href="AllProducts.php">Todos los productos</a>
-                        <a href="#vision">Tendencias</a>
-                        <a href="#valores">Nuevo</a>
-                    </div>
-                </li>
-                <li class="dropdown">
-                    <a href="Login.php" class="dropbtn">Usuarios</a>
-                    <div class="dropdown-content">
-                        <a href="SignUp.php">Crear cuenta</a>
-                        <a href="Login.php">Iniciar sesion</a>
-                    </div>
-                </li>
-            </ul>
-            
-         <a href="chekout.php" class="btn btn-primary m-2" type="submit">
-            <i class="bi-cart-fill me-1"></i>
-            Carrito
-            <span id="num_cart" class="badge bg-secondary"></span>
-        </a>
-        <?php if (isset($_SESSION['user_id'])) { ?>
-            <a href="#" class="btn btn-success">
-                <i class="bi bi-person-circle"></i>
-                <?php echo $_SESSION['user_name']; ?>
-            </a>
-        <?php } else { ?>
-            <a href="login.php" class="btn btn-success">
-                <i class="bi bi-person-circle"></i>
-                Ingresar
-            </a>
-        <?php } ?>
-    </nav>
-
+    <?php include 'menu.php'; ?>
+    
     <!-- Header-->
     <header class="headercontenedor py-5">
         <div class="container px-4 px-lg-5 my-5">
