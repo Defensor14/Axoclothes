a0a2919bbcf12c3b020c026e61f6281e5003505a
AaronGomezC <168867785+AaronGomezC@users.noreply.github.com>
2024-05-28 09:04:14 -0600

    Footer_Registro
    Footer simple para paginas de registro (Signup, Login, Reset, etc.)

---
+++ home/footer_registro.php
@@ -0,0 +1,5 @@
+<footer class="py-5 bg-dark" style="height: 100px;">
+        <div class="container mt-3">
+            <p class="m-0 text-center text-white">© Copyright 2023 - 2024 Axoclothes Inc. Todos los derechos reservados.</p>
+        </div>
+</footer>
