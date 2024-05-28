22b388e91fcbb2c9e88ae20ef94e6f90d37e5df7
AaronGomezC <168867785+AaronGomezC@users.noreply.github.com>
2024-05-28 09:16:01 -0600

    Update Login.php
    -1 solo navbar

--- home/Login.php
+++ home/Login.php
@@ -58,21 +58,6 @@ if (!empty($_POST)) {
 <body style="background-color: #9c9c9c;">
     <!-- Navigation-->
     <?php include 'menu_registro.php';?>
-
-    <nav class="navbar">
-        <div id="logo">
-            <img src="images/logo.svg">
-        </div>
-        <a class="navbar-brand" href="index.php">AXOCLOTHES</a>
-        <button class="navbar-toggler btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <spanclass="navbar-toggler-icon"></span></button>
-        <ul class="menu">
-            <li><a href="index.php">Inicio</a></li>
-            <li><a href="window.history.back()">Regresar</a></li> 
-        </ul>
-    </nav>
-
     <!-- Header-->
     
 
