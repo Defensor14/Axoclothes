4d2d5f9b8b7ee179eb85d11b231a3c7c5726d9c6
AaronGomezC <168867785+AaronGomezC@users.noreply.github.com>
2024-05-28 11:18:42 -0600

    Update reset_password.php
    Background Image

--- home/reset_password.php
+++ home/reset_password.php
@@ -70,7 +70,7 @@ if (!empty($_POST)) {
 
 </head>
 
-<body style="background-color: #9c9c9c;">
+<body style="background-image: url('https://static.vecteezy.com/system/resources/previews/010/839/386/non_2x/aesthetic-minimal-cute-pastel-pink-wallpaper-illustration-perfect-for-wallpaper-backdrop-postcard-background-banner-vector.jpg');">
     <!-- Navigation-->
     <?php include 'menu_registro.php';?>
 
